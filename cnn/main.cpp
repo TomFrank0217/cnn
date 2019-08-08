15dc90617a227aae6d2acc4142e1215ec6deafa8
TomFrank0217 <1481985686@qq.com>
2019-08-08 16:53:02 +0800

    m

--- cnn/main.cpp
+++ cnn/main.cpp
@@ -40,6 +40,7 @@ int main(int argc, char* argv[]){
 
 
 
+
     string file_name = "F:\\chormeDownload\\trainimage\\pic2\\0\\*.bmp";
     vector<num_path> vec_path_label;
     get_image_path_and_label(vec_path_label, file_name);
