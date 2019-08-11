c1184d7ba920dde8a106c1e2cfb1e448337acfb1
TomFrank0217 <1481985686@qq.com>
2019-08-11 16:52:06 +0800

    add features2matrix in features

--- cnn/features.cpp
+++ cnn/features.cpp
@@ -30,19 +30,42 @@ bool features::reshape(int kernels_rows, int kernels_cols, int stride, int paddi
 		return false;
 	}
 
-	m_kernels_rows = kernels_rows;
-	m_kernels_cols = kernels_cols;
+	m_kernel_rows = kernels_rows;
+	m_kernel_cols = kernels_cols;
 	m_stride = stride;
 	m_padding_mode = padding_mode;
+	/* 以下三个变量来确定卷积左上方元素的在kernel中的位置 */
+	int channel_in_kernels = 0;/* channel 代表第几个卷积核(特征) */
+	int row_in_kernel = 0;     /* row代表元素相对于卷积核左上方元素的行数 */
+	int col_in_kernel = 0;     /* col代表元素相对于卷积核左上方元素的列数 */
+	/* 以上三个变量来确定单个卷积核左上方元素的在kernel中的位置 */
 
+	/* 以下两个变量来确定卷积核左上角元素相对于单个特征的位置 */
+	int row_in_feature = 0;
+	int col_in_feature = 0;
+	/* 仿照牛顿力学中的相对位置，即坐标转换，\
+	   能够确定features_matrix元素在原始features中的绝对位置 */
+	int index_in_kernel = 0;
+	/* index_in_kernels辅助变量，描述元素相对于单个卷积核的相对位置（一维）*/
 	switch (padding_mode){
-	case VALID_PADDING: /* (N+2P-K)/S+1 */
-		m_features_matrix = matrix( ( (m_rows - m_kernels_rows) / stride+1 ) \
-                        		  * ( (m_cols - m_kernels_cols) / stride+1 ), \
-			                        m_kernels_rows*m_kernels_cols*m_channels);
+	case VALID_PADDING: /* (N+2P-K)/S +1 */
+		m_features_matrix = matrix( ((m_rows - m_kernel_rows)/stride +1)* \
+                        		    ((m_cols - m_kernel_cols)/stride +1), \
+			                        m_kernel_rows*m_kernel_cols*m_channels);
+		/* 下面是核心代码 features2matrix */
+		/* todo 此处计算可以优化 */
 		for (int i = 0; i < m_features_matrix.m_rows; ++i){
 			for (int j = 0; j < m_features_matrix.m_cols; ++j){
-
+				channel_in_kernels = j % (m_kernel_rows*m_kernel_cols);
+				index_in_kernel = j - channel_in_kernels*m_kernel_rows*m_kernel_cols;
+				row_in_kernel = index_in_kernel%m_kernel_cols;
+				col_in_kernel = index_in_kernel - row_in_kernel*m_kernel_cols;
+				
+				row_in_feature = i % ((m_cols - m_kernel_cols) / stride + 1);
+				col_in_feature = i - row_in_feature*((m_cols - m_kernel_cols) / stride + 1);
+				m_features_matrix.mp_data[i*m_features_matrix.m_cols + j] = \
+					mp_matrixes[channel_in_kernels].mp_data\
+					[(row_in_feature + row_in_kernel)*m_rows + (col_in_feature + col_in_kernel)];
 			}
 		}
 		break;
